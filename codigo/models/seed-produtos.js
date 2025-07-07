f79209c54790179e183dd5d295cd8a2c2ff1f64b
Augusto Miotto <augustommiotto@gmail.com>
2025-07-07 08:47:40 -0300

    Seed de produtos corrigida

--- codigo/models/seed-produtos.js
+++ codigo/models/seed-produtos.js
@@ -33,42 +33,42 @@ async function seedProdutos() {
     {
       nome: 'Espumante Chandon Réserve Brut 750ml',
       preco: 69.90,
-      imagem: 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-luqlsdike6w0a2',
+      imagem: 'https://m.media-amazon.com/images/I/51MYsJkhjhL.jpg',
       descricao: 'Espumante leve, refrescante e elegante.',
       categoria: 'Espumante'
     },
     {
       nome: 'Licor Amarula 750ml',
       preco: 59.90,
-      imagem: 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-luqlsdike6w0a2',
+      imagem: 'https://carrefourbrfood.vtexassets.com/arquivos/ids/107281595/licor-amarula-750-ml-1.jpg?v=638150850822270000',
       descricao: 'Licor cremoso de fruta africana marula.',
       categoria: 'Licor'
     },
     {
       nome: 'Rum Bacardi Carta Blanca 980ml',
       preco: 49.90,
-      imagem: 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-luqlsdike6w0a2',
+      imagem: 'https://images.tcdn.com.br/img/img_prod/584235/rum_bacardi_carta_blanca_garrafa_980ml_438983646_1_dbbcca2b7f42d5fc047d1db5578bad5b_20250317122741.jpg',
       descricao: 'Rum branco clássico, perfeito para drinks.',
       categoria: 'Rum'
     },
     {
       nome: 'Saquê Azuma Kirin 740ml',
       preco: 29.90,
-      imagem: 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-luqlsdike6w0a2',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-eGiUrIx79M3AMNGWDx98__WK0-Vs3rm0nQ&s',
       descricao: 'Saquê tradicional japonês, suave e aromático.',
       categoria: 'Saquê'
     },
     {
       nome: 'Gin Tanqueray 750ml',
       preco: 89.90,
-      imagem: 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-luqlsdike6w0a2',
+      imagem: 'https://www.imigrantesbebidas.com.br/bebida/images/products/full/9095-gin-tanqueray-dry-750ml.20230807092325.jpg',
       descricao: 'Gin clássico, ideal para gin tônica.',
       categoria: 'Gin'
     },
     {
       nome: 'CHEVROLET MONZA GLS 2.0 EFI 4P 1996',
       preco: 21.900,
-      imagem: 'https://down-br.img.susercontent.com/file/br-11134207-7r98o-luqlsdike6w0a2',
+      imagem: 'https://www.chavesnamao.com.br/imn/0400x0350/A/60/veiculos/340017/7475156/chevrolet-monza-em-curitiba-pr-bf632f76.jpg',
       descricao: 'Monza GLS 2.0 EFI 1996. Placa A; Direção hidráulica; Vidros elétricos nas 4 portas; Rodas de liga leve; Documento ok.',
       categoria: 'Destilado'
     }
